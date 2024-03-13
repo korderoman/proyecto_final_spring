a80ce7e9cf3599dec58250d749485ba0721d9081
PantaX <pantajefferson173@gmail.com>
2024-03-12 20:58:44 -0500

    relacion de tablas

--- common/databases/database.sql
+++ common/databases/database.sql
@@ -1,26 +1,8 @@
--- Crear tabla login_password
-DROP TABLE IF EXISTS login_password;
-CREATE TABLE login_password (
-                                id_cliente SERIAL PRIMARY KEY,
-                                usuario VARCHAR(20) NOT NULL,
-                                tipo_cliente VARCHAR(50) NOT NULL,
-                                password VARCHAR(255) NOT NULL,
-                                estado INT NOT NULL,
-                                usua_crea VARCHAR(45),
-                                date_create TIMESTAMP,
-                                usua_modif VARCHAR(45),
-                                date_modif TIMESTAMP,
-                                usua_delet VARCHAR(45),
-                                date_delet TIMESTAMP
-);
-
--- Crear tabla cliente_persona
 DROP TABLE IF EXISTS cliente_persona;
 CREATE TABLE cliente_persona (
-                                 id_cliente SERIAL PRIMARY KEY,
-                                 tipo_usuario VARCHAR(50) NOT NULL,
+                                 id_cliente SERIAL PRIMARY key,
                                  dni VARCHAR(20) NOT NULL,
-                                 nombre VARCHAR(100) NOT NULL,
+                                 nombres VARCHAR(100) NOT NULL,
                                  ape_paterno VARCHAR(100) NOT NULL,
                                  ape_materno VARCHAR(100) NOT NULL,
                                  estado INT NOT NULL,
@@ -29,33 +11,15 @@ CREATE TABLE cliente_persona (
                                  usua_modif VARCHAR(45),
                                  date_modif TIMESTAMP,
                                  usua_delet VARCHAR(45),
-                                 date_delet TIMESTAMP
-);
-
--- Crear tabla cliente_empresa
-DROP TABLE IF EXISTS cliente_empresa;
-CREATE TABLE cliente_empresa (
-                                 id_cliente SERIAL PRIMARY KEY,
-                                 ruc VARCHAR(20) NOT NULL,
-                                 razon_social VARCHAR(255) NOT NULL,
-                                 codigo_envio VARCHAR(50) NOT NULL,
-                                 nombre_comercial VARCHAR(255) NOT NULL,
-                                 fec_ini_vlg TIMESTAMP,
-                                 fec_fin_vlg TIMESTAMP,
-                                 estado INT NOT NULL,
-                                 usua_crea VARCHAR(45),
-                                 date_create TIMESTAMP,
-                                 usua_modif VARCHAR(45),
-                                 date_modif TIMESTAMP,
-                                 usua_delet VARCHAR(45),
-                                 date_delet TIMESTAMP
+                                 date_delet TIMESTAMP,
+                                 CONSTRAINT uq_cliente_dni UNIQUE (dni)
 );
 
 -- Crear tabla direccion
 DROP TABLE IF EXISTS direccion;
 CREATE TABLE direccion (
-                           id_cliente SERIAL,
                            id_direccion SERIAL PRIMARY KEY,
+                           id_cliente INT ,
                            direccion TEXT NOT NULL,
                            ciudad VARCHAR(100) NOT NULL,
                            provincia VARCHAR(100) NOT NULL,
@@ -73,37 +37,55 @@ CREATE TABLE direccion (
                            usua_modif VARCHAR(45),
                            date_modif TIMESTAMP,
                            usua_delet VARCHAR(45),
-                           date_delet TIMESTAMP
+                           date_delet TIMESTAMP,
+                           CONSTRAINT uq_direccion_email UNIQUE (email),
+                           FOREIGN KEY (id_cliente) REFERENCES cliente_persona(id_cliente) ON DELETE SET NULL
 );
 
--- Crear tabla hist_pedido
-DROP TABLE IF EXISTS hist_pedido;
-CREATE TABLE hist_pedido (
-                             id_cliente SERIAL,
-                             id_pedido SERIAL PRIMARY KEY,
-                             fecha_inicio_pedido TIMESTAMP,
-                             estado INT NOT NULL,
-                             usua_crea VARCHAR(45),
-                             date_create TIMESTAMP,
-                             usua_modif VARCHAR(45),
-                             date_modif TIMESTAMP,
-                             usua_delet VARCHAR(45),
-                             date_delet TIMESTAMP
+DROP TABLE IF EXISTS rol;
+CREATE TABLE rol (
+                     id_rol SERIAL primary KEY,
+                     nombre_rol VARCHAR(100),
+                     estado INT NOT NULL,
+                     usua_crea VARCHAR(45),
+                     date_create TIMESTAMP,
+                     usua_modif VARCHAR(45),
+                     date_modif TIMESTAMP,
+                     usua_delet VARCHAR(45),
+                     date_delet TIMESTAMP,
+                     CONSTRAINT uq_rol_nombre UNIQUE (nombre_rol)
 );
 
--- Crear tabla hist_prod_pedido
-DROP TABLE IF EXISTS hist_prod_pedido;
-CREATE TABLE hist_prod_pedido (
-                                  id_pedido SERIAL,
-                                  id_prod SERIAL,
-                                  fecha_ingreso TIMESTAMP,
-                                  estado INT NOT NULL,
-                                  usua_crea VARCHAR(45),
-                                  date_create TIMESTAMP,
-                                  usua_modif VARCHAR(45),
-                                  date_modif TIMESTAMP,
-                                  usua_delet VARCHAR(45),
-                                  date_delet TIMESTAMP
+drop table if exists rol_cliente;
+create table rol_cliente(
+                            id_rol_cliente SERIAL primary KEY,
+                            id_rol int,
+                            id_cliente int,
+                            estado INT NOT NULL,
+                            usua_crea VARCHAR(45),
+                            date_create TIMESTAMP,
+                            usua_modif VARCHAR(45),
+                            date_modif TIMESTAMP,
+                            usua_delet VARCHAR(45),
+                            date_delet TIMESTAMP,
+                            FOREIGN KEY (id_rol) REFERENCES rol(id_rol),
+                            FOREIGN KEY (id_cliente) REFERENCES cliente_persona(id_cliente)
+);
+
+CREATE TABLE usuario (
+                         id_user SERIAL primary KEY,
+                         id_cliente int,
+                         usuario VARCHAR(20) NOT NULL,
+                         password VARCHAR(255) NOT NULL,
+                         estado INT NOT NULL,
+                         usua_crea VARCHAR(45),
+                         date_create TIMESTAMP,
+                         usua_modif VARCHAR(45),
+                         date_modif TIMESTAMP,
+                         usua_delet VARCHAR(45),
+                         date_delet TIMESTAMP,
+                         CONSTRAINT uq_usuario_nombre UNIQUE (usuario),
+                         FOREIGN KEY (id_cliente) REFERENCES cliente_persona(id_cliente) ON DELETE SET NULL
 );
 
 -- Crear tabla pedido
@@ -121,7 +103,39 @@ CREATE TABLE pedido (
                         usua_delet VARCHAR(45),
                         date_delet TIMESTAMP
 );
+DROP TABLE IF EXISTS hist_pedido;
+CREATE TABLE hist_pedido (
+                             id_hist_pedido SERIAL primary key,
+                             id_cliente int,
+                             id_pedido int,
+                             fecha_inicio_pedido TIMESTAMP,
+                             estado INT NOT NULL,
+                             usua_crea VARCHAR(45),
+                             date_create TIMESTAMP,
+                             usua_modif VARCHAR(45),
+                             date_modif TIMESTAMP,
+                             usua_delet VARCHAR(45),
+                             date_delet TIMESTAMP,
+                             FOREIGN KEY (id_cliente) REFERENCES cliente_persona(id_cliente) ON DELETE SET NULL,
+                             FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido) ON DELETE SET NULL
+);
 
+
+-- Crear tabla hist_prod_pedido
+DROP TABLE IF EXISTS hist_prod_pedido;
+CREATE TABLE hist_prod_pedido (
+                                  id_prod_pedido SERIAL primary key,
+                                  id_hist_pedido int,
+                                  fecha_ingreso TIMESTAMP,
+                                  estado INT NOT NULL,
+                                  usua_crea VARCHAR(45),
+                                  date_create TIMESTAMP,
+                                  usua_modif VARCHAR(45),
+                                  date_modif TIMESTAMP,
+                                  usua_delet VARCHAR(45),
+                                  date_delet TIMESTAMP,
+                                  FOREIGN KEY (id_hist_pedido) REFERENCES hist_pedido(id_hist_pedido) ON DELETE SET NULL
+);
 -- Crear tabla prod_pedido
 DROP TABLE IF EXISTS prod_pedido;
 CREATE TABLE prod_pedido (
@@ -256,29 +270,4 @@ CREATE TABLE precio (
                         date_delet TIMESTAMP
 );
 
-DROP TABLE IF EXISTS rol;
-CREATE TABLE rol (
-                     id_rol SERIAL,
-                     nombre_rol VARCHAR(100),
-                     estado INT NOT NULL,
-                     usua_crea VARCHAR(45),
-                     date_create TIMESTAMP,
-                     usua_modif VARCHAR(45),
-                     date_modif TIMESTAMP,
-                     usua_delet VARCHAR(45),
-                     date_delet TIMESTAMP
-);
 
-drop table if exists rol_cliente;
-create table rol_cliente(
-                            id_rol_cliente SERIAL,
-                            id_rol int,
-                            id_cliente int,
-                            estado INT NOT NULL,
-                            usua_crea VARCHAR(45),
-                            date_create TIMESTAMP,
-                            usua_modif VARCHAR(45),
-                            date_modif TIMESTAMP,
-                            usua_delet VARCHAR(45),
-                            date_delet TIMESTAMP
-)
