cda1b0db9e5211b870a301a5ad67f18d1dd86e5b
PantaX <pantajefferson173@gmail.com>
2024-03-28 18:17:59 -0500

    seguridad: implementacion de reniec para el registro

--- application/src/main/java/org.grupo3.proyectofinalspring.application/ApplicationLauncher.java
+++ application/src/main/java/org.grupo3.proyectofinalspring.application/ApplicationLauncher.java
@@ -1,8 +1,11 @@
 package org.grupo3.proyectofinalspring.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,7 +14,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("org.grupo3.*")
 @EntityScan("org.grupo3.*")
 @EnableJpaRepositories("org.grupo3")
-
+@EnableFeignClients("org.grupo3.*")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class ApplicationLauncher {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationLauncher.class, args);
